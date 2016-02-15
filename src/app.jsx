0203ddbd9fe8331118366d5ccc359eda448e4192
Danny Rizko <drizko07@gmail.com>
2016-02-15 11:26:30 -0800

    Initial Commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var SoundBoard = require('./components/soundboard.jsx')
+
+var App = React.createClass({
+  render: function() {
+    return <SoundBoard />
+  }
+});
+
+var element = React.createElement(App, {});
+ReactDOM.render(element, document.querySelector('.container'));
