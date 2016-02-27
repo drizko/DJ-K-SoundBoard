166ae48061f78fa64a4d6635621d2f2ff3afbdbb
Danny Rizko <drizko07@gmail.com>
2016-02-27 15:58:07 -0800

    Changes Procfile

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static('./'));
 
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/'));
+})
+
 app.listen(port);
 
 console.log('Express server listening on %d in %s mode', port, app.settings.env);
